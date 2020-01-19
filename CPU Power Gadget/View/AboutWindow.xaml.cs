0e7774cc0106683e45105bce0ec5575f99060fc5
Tintanyul <57907557+tintanyul@users.noreply.github.com>
2020-01-19 11:24:42 +0100

    Dynamic version in About window.

--- CPU Power Gadget/View/AboutWindow.xaml.cs
+++ CPU Power Gadget/View/AboutWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Input;
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Windows.Input;
 
 namespace CpuPowerGadget.View
 {
@@ -12,6 +14,17 @@ namespace CpuPowerGadget.View
             {
                 if (args.Key == Key.Escape) Close();
             };
+        }
+
+        public static string Version
+        {
+            get
+            {
+                var assembly = Assembly.GetExecutingAssembly();
+                var fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+                var version = fileVersionInfo.ProductVersion;
+                return version;
+            }
         }
 
         private void OnLegalClick(object sender, System.Windows.RoutedEventArgs e)
